ce8d59fa1504a96480faca1576bf733a23807b68
Ualter Azambuja <ualter.junior@gmail.com>
2018-09-12 22:48:13 +0200

    .

--- socketChat/SocketIonicClient/src/app/app.module.ts
+++ socketChat/SocketIonicClient/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, Injectable } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
 import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+import { Socket } from 'ng-socket-io';
 
 // https://github.com/bougarfaoui/ng-socket-io
 const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
+export class SocketXpPlugin extends Socket {
+  constructor(public UrlXpPlugin: string) {
+    super({url: UrlXpPlugin, options: {} });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +25,8 @@ const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    SocketIoModule.forRoot(config)
+    //SocketIoModule.forRoot(config)
+    SocketIoModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -30,6 +36,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
   providers: [
     StatusBar,
     SplashScreen,
+    SocketXpPlugin,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- socketChat/SocketIonicClient/src/pages/home/home.ts
+++ socketChat/SocketIonicClient/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Socket } from 'ng-socket-io';
+import { SocketXpPlugin } from '../../app/app.module';
 
 @Component({
   selector: 'page-home',
@@ -10,11 +11,14 @@ export class HomePage {
 
   nickname = '';
 
-  constructor(public navCtrl: NavController, private socket: Socket) {
+  constructor(public navCtrl: NavController, private socket: Socket, private socketXpPlugin: SocketXpPlugin) {
 
   }
 
   joinChat() {
+
+    this.socketXpPlugin = new SocketXpPlugin("http://192.168.0.22:3001");
+
     this.socket.connect();
     this.socket.emit('set-nickname', this.nickname);
     this.navCtrl.push('ChatRoomPage', { nickname: this.nickname });
